7d3249dfdc25f056f3f8b1f9b8d29e343f87c226
PradeeshSuganthan <pradeesh.suganthan@gmail.com>
2017-05-03 17:25:39 -0400

    Implemented testing, random label guess

--- linear_classifier.py
+++ linear_classifier.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import gzip
 import struct
-
+import random
 
 def readData():
 	image_train_filename = 'data/train-images-idx3-ubyte.gz'
@@ -13,20 +13,20 @@ def readData():
 
 	#uncompress files and read data
 	with gzip.open(image_train_filename, 'r') as f:
-		magicnum, numimage, row, col = struct.unpack('>IIII',f.read(16))
-		images = np.fromstring(f.read(), dtype='uint8').reshape(numimage,row, col)
+		magicnum, numimage, row, col = struct.unpack('>IIII', f.read(16))
+		images = np.fromstring(f.read(), dtype='uint8').reshape(numimage, row, col)
 
 	with gzip.open(label_train_filename, 'r') as f:
-		magicnum, numlabel = struct.unpack('>II',f.read(8))
-		labels = np.fromstring(f.read(),dtype='int8')
+		magicnum, numlabel = struct.unpack('>II', f.read(8))
+		labels = np.fromstring(f.read(), dtype='int8')
 
 	with gzip.open(image_test_filename, 'r') as f:
-		magicnum, numimage, row, col = struct.unpack('>IIII',f.read(16))
-		images_t = np.fromstring(f.read(), dtype='uint8').reshape(numimage,row, col)
+		magicnum, numimage, row, col = struct.unpack('>IIII', f.read(16))
+		images_t = np.fromstring(f.read(), dtype='uint8').reshape(numimage, row, col)
 
 	with gzip.open(label_test_filename, 'r') as f:
-		magicnum, numlabel = struct.unpack('>II',f.read(8))
-		labels_t = np.fromstring(f.read(),dtype='int8')
+		magicnum, numlabel = struct.unpack('>II', f.read(8))
+		labels_t = np.fromstring(f.read(), dtype='int8')
 
 
 	return images, images_t, labels, labels_t
@@ -40,20 +40,37 @@ def trainClassifier():
 
 
 
-def testClassifier():
-
+def testClassifier(images, labels):
+	correct = 0
+	total = 0
+	guess = []
 
 	print "Testing"
 
+	#Guess a random number for each image
+	for i in images:
+		guess.append(random.randint(0,9))
+
+
+	#Check accuracy of guess
+	for i in range(0,len(guess)):
+		if guess[i] == labels[i]:
+			correct += 1
+
+		total += 1
+
+	accuracy = (correct/ float(total))*100
+
+	return accuracy
 
 
 
 def main():
 	images_train, images_test, labels_train, labels_test = readData()
 
+	accuracy = testClassifier(images_test, labels_test)
 
-
-
+	print "Accuracy = " + str(accuracy) + "%"
 
 
 
